28d5e903c6ac7a47d0ce9867211f8669283db398
sojerton <32437480+sojerton@users.noreply.github.com>
2017-10-05 01:14:24 +0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int farlongtolb(int);
+int main()
+{
+	int(farlong);
+	cout << "farlongs: ";
+	cin >> farlong;
+	int yards = farlongtolb(farlong);
+	cout << farlong << " farlongs = ";
+	cout << yards <<" yards";
+	return 0;
+}
+int farlongtolb(int chet)
+{
+	return 220 * chet;
+}
