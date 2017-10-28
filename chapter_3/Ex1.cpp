34dcdcff1c20bc62c8dd9193cfccfa34d90c52fd
Artem Matveev <32437480+sojerton@users.noreply.github.com>
2017-10-28 13:48:03 +0300

    Update Ex1.cpp

--- chapter_3/Ex1.cpp
+++ chapter_3/Ex1.cpp
@@ -5,9 +5,9 @@ const int ft = 12;
 
 int main()
 {
-	
+	setlocale(LC_ALL, "Russian");
 	int growth;
-	cout << "Enter your фвывфывф growth: ___\b\b\b";
+	cout << "Enter your growth: ___\b\b\b";
 	cin >> growth;
 	cout << "Your growth: " << growth / ft << " foot and " << (growth % ft) << " inch\n";
 	system("pause");
