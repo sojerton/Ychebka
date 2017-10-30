0265db8c581c828e4f7a27eababd9ba8e8f194fd
Artem Matveev <32437480+sojerton@users.noreply.github.com>
2017-10-30 19:13:59 +0300

    Update Ex1.cpp

--- Chapter_4/Ex1.cpp
+++ Chapter_4/Ex1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
 
 using namespace std;
 
-int main(){
-  char firstName, lastName, grade, age;
-  cout << "What is your first name? ";
-  cin.getline(firstName, 25);
-  cout << "Whant is your last name? ";
-  cin >> lastName >> endl;
-  cout << "What letter grade do you deserve? ";
-  cin >> grade >> endl;
-  cout << "What is your age? ";
-  cin >> age >> endl;
-  
-  cout << "Name: " << lastName << ", " << firstName << endl;
-  cout << "Grade: " << grade << endl;
-  cout << "Age: " << age;
-  system("pause");
-  return 0;  
+int main() {
+	char firstName[30], lastName[15], grade[2], age[4];
+	cout << "What is your first name? ";
+	cin.getline(firstName, 30);
+	cout << "Whant is your last name? ";
+	cin >> lastName;
+	cout << "What letter grade do you deserve? ";
+	cin >> grade;
+	cout << "What is your age? ";
+	cin >> age;
+
+	cout << "\n";
+	cout << "Name: " << lastName << ", " << firstName << endl;
+	cout << "Grade: " << grade << endl;
+	cout << "Age: " << age << "\n" << endl;
+	system("pause");
+	return 0;
 }
