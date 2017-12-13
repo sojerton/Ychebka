6d788571b021d30bb8a21df4b0ae60ed4333a4b1
Artem Matveev <32437480+sojerton@users.noreply.github.com>
2017-12-13 18:20:49 +0300

    Update main.cpp

--- labs/lab8.10/main.cpp
+++ labs/lab8.10/main.cpp
@@ -1,50 +1,100 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <locale>
- 
+#include <cstdlib>
+#include <conio.h>
+#include <cstring>
+
 using namespace std;
 
 struct catalog {
-    char name[3];
-    char data[3];
+    char name[10];
+    struct dataInf{int day, month, year;
+    } data;
     int num;
-    catalog* next;
+    bool operator<(catalog in2)
+{
+   return (num < in2.num);
+}
 };
+
+#define SIZE 3
 
 void Menu(void){
     system("cls");
-    cout << "Меню"<<endl;
-    cout << "1. Вывод каталога на экран."<<endl;
-    cout << "2. Сортировка по дате."<<endl;
-    cout << "3. Выборка по кол-ву обращений."<<endl;
-    cout << "Esc для выхода."<<endl;
+    cout << "memset(img, 0, sizeof(img))Меню"<<endl;
+    cout << "1. Создание каталога."<<endl;
+    cout << "2. Вывод каталога на экран."<<endl;
+    cout << "3. Сортировка по дате."<<endl;
+    cout << "4. Выборка по кол-ву обращений."<<endl;
+    cout << "5. для выхода."<<endl;
 }
 
 main(){
     setlocale(LC_ALL,"rus");
-    srand(time(NULL));
-    catalog in;
-        in.name[3]={"qqq","www","eee"};
-        in.num[3]={"11","23","3"};
-        in.data[3]={"12.11.2012","19.03.2014","23.06.2013"};
-    char key=0;
+    catalog in[SIZE];
     Menu();
-        switch (key){
-        case '1';
-            for (int i=0; i<3; i++){
-                cin >>  in.name >>  in.num >> in.data;
+    int key, p;
+    cin >>key;
+     int k=(SIZE+1);
+    bool A=true;
+    while (A==true){
+        if (key<6 && key>0) {
+            switch (key){
+            case 1:
+                    cout <<"Создание коталога...\n";
+                        for (int i=0; i<SIZE; i++){
+                            cout << "Введите имя файла: ";
+                            cin >>  in[i].name;
+                            cout << "Введите дату создания файла: \n";
+                              cout << "День: "; cin>>in[i].data.day;
+                              cout << "Месяц: "; cin>>in[i].data.month;
+                              cout << "Год: "; cin>>in[i].data.year;
+                            cout << "Введите кол-во обращений к файлу: ";
+                            cin >> in[i].num;
+                            cout<<endl;
+                        }
+                    Menu();
+                    cin>>key;
+                    break;
+             case 2:
+                for (int i=0; i<SIZE; i++){
+                    cout<<"Файл №"<<i+1<<endl;
+                    cout<<in[i].name<<endl;;
+                    cout<<in[i].data.day<<"."<<in[i].data.month<<"."<<in[i].data.year<<endl;
+                    cout<<in[i].num<<" Обращения.\n"<<endl;
+                }
+                cin>>key;
+                break;
+             case 3:
+                cout<<"Введите дату для сортировки: ";
+                  cout << "День: "; cin>>in[k].data.day;
+                  cout << "Месяц: "; cin>>in[k].data.month;
+                  cout << "Год: "; cin>>in[k].data.year;
+                for (int i=0; i<SIZE; i++){
+                    if (in[k].data.year>in[i].data.year)
+                        if (in[k].data.month>in[i].data.month)
+                            if (in[k].data.day>in[i].data.day)
+                               memset(&in[i], 0, sizeof(catalog));
+                }
+                cin>>key;
+                break;
+             case 4:
+                for (int i=1; i<SIZE; i++){
+                    if (in[i-1].num>in[i].num)
+                        in[k].num=in[i-1].num;
+                        p=i-1;
+                    }
+                    cout<<"Файл №"<<p+1<<endl;
+                    cout<<in[p].name<<endl;;
+                    cout<<in[p].data.day<<"."<<in[p].data.month<<"."<<in[p].data.year<<endl;
+                    cout<<in[p].num<<" Обращения.\n"<<endl;
+                cin>>key;
+                break;
+              case 5:   exit(0);
             }
-            break;
-         case '2';
-            
-            break;
-         case '3';
-            
-            break;
-        }while ((key=getch())!="Esc");
-   
-   
+        }
+    else {cout<<"Неверное значение"<<endl;
+    A=false;}
+    }
     cin.get();
     return 0;
 }
