6afeb5bc47e6f9d7d7cc277adaf4ec809fb50d2b
Artem Matveev <32437480+sojerton@users.noreply.github.com>
2018-03-19 03:01:16 +0300

    Update classes.cpp

--- semester_2/classes.cpp
+++ semester_2/classes.cpp
@@ -3,113 +3,51 @@
 
 using namespace std;
 
-static string army="English";
-static string insurgents="Polish";
+static string benzine_1="80";
+static string benzine_2="92";
+static string benzine_3="95";
+static string benzine_4="ДТ";
 
-class Unit
+class Car
 {
+private:
+    bool typeFuel;
 public:
-    int health;
-    Unit():health(10){}
-    Unit(int a): health(a){}
-    Unit(const Unit &other){this->health=other.health;}
-    void showHealth(){cout<<"Unit health: "<<health<<endl;};
-    ~Unit(){}
+    Car():typeFuel(true);
+    void showType(){if(typeFuel)cout<<benzine_2<<endl;
+                    else cout<<benzine_4<<endl;}
+    ~Car(){}
 };
 
-class Soldier: virtual public Unit
+class passenger cars : virtual public Car
 {
-protected:
-    int speed;
 public:
-    int damage;
-    Soldier():damage(20),speed(10){}
-    Soldier(int a, int b, int c):Unit(a),damage(b),speed(c) { }
-    void nation(){cout<<insurgents<<endl;}
-    void showDamage(){cout<<"soldier damage: "<<damage<<endl;}
-    void showSpeed(){cout<<"Soldier speed: "<<speed<<endl;}
-
-    class citizen: virtual public Unit
+    
+    
+    class bus
     {
-    public:
-        void showHealth(){cout<<"hp fermer is: "<<health<<endl;}
+        
     };
-
-    ~Soldier(){}
 };
 
-class Horse: virtual public Unit
+class trucks : virtual public Car
 {
-protected:
-    int speed;
 public:
-    Horse():speed(20){}
-    Horse(int a):speed(a){}
-    void showSpeed(){cout<<"Horse speed: "<<speed<<endl;}
-    ~Horse(){}
+    
+    
+    
 };
 
-class Horseman :public Horse, public Soldier
-{
-private:
-    int speed;
-public:
-    Horseman():speed(Horse::speed+Soldier::speed){}
-    void nation(){cout<<army<<endl;}
-    void showDamage(){cout<<"Horseman damage: "<<damage<<endl;}
-    void showSpeed(){cout<<"Horseman speed: "<<speed<<endl;}
-    ~Horseman(){}
-};
 
-class Battle :public Horseman
-{
-private:
-    int Unit_1;
-public:
-    Battle():Unit_1(0){}
-    Battle(int first): Unit_1(first){}
-    void showBattle()
-    {
-        cout<<"zhannaDark caused "<<damage<<" damage."<<endl;
-        cout<<"achilles left "<<Unit_1<<" hp."<<endl;
-    }
 
-    friend Battle operator- (Battle&l, Battle& u1);
-    ~Battle(){}
-};
 
-Battle operator- (Battle&l, Battle& u1)
-    {
-        return Battle (l.Unit_1-u1.Unit_1);
-    }
 
 int main()
 {
-    Soldier achilles(49,5,15);
-    achilles.nation();
-    achilles.showHealth();
-    achilles.showDamage();
-    achilles.showSpeed();
-
-    cout<<endl;
-    Horse sivka(40);
-    sivka.showHealth();
-    sivka.showSpeed();
-    cout<<endl;
-
-    Horseman zhannaDark;
-    zhannaDark.nation();
-    zhannaDark.showHealth();
-    zhannaDark.showDamage();
-    zhannaDark.showSpeed();
-    cout<<endl;
-    Battle Unit1(achilles.health);
-    Battle Unit2(zhannaDark.damage);
-    Battle Unit3 = Unit1 - Unit2;
-    Unit3.showBattle();
-    cout<<endl;
-    Soldier::citizen fermer;
-    fermer.showHealth();
+    setlocale(0, "rus");
     
+    
+    
+
     return 0;
 }
