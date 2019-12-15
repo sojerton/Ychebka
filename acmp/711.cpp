e31bde30bac96394a63c6e7780a314abee821e50
Artem Matveev <art.matveef2012@yandex.ru>
2019-12-15 14:38:27 +0300

    Update 711.cpp

--- acmp/711.cpp
+++ acmp/711.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int main() {
-	long n, m, temp, sum = 100001, min;
-	(cin >> n >> m).get();
-	long *circle = new long[n];
-	string *str = new string[n];
-	for (int i = 0; i < n; i++) {
-		cin >> str[i];
-		circle[i] = 0;
-		for (int j = 0; j < m; j++) {
+	long n, m, temp, sum = 100001, min; //n,m - входные данные, temp - переменная времени каждого круга, sum - общая сумма времени кругов, min - индекс участника
+	(cin >> n >> m).get(); //считываем первую строку
+	long *circle = new long[n]; //сумма времени кругов(динамическая)
+	string *str = new string[n]; //динамический массив строк участников
+	for (int i = 0; i < n; i++) { 
+		cin >> str[i]; //считываем имя
+		circle[i] = 0; //обнуляем счетчик для каждого участника 
+		for (int j = 0; j < m; j++) { //суммируем время
 			(cin >> temp).get();
 			circle[i] += temp;
 		}
-		if (sum > circle[i]) { sum = circle[i]; min = i; }
+		if (sum > circle[i]) { sum = circle[i]; min = i; } //если бщее время меньше минимального обновляем
 	}
-	cout << str[min];
+	cout << str[min]; //выводим участника с минимальным временем
 	system("pause");
 	return 0;
 }
